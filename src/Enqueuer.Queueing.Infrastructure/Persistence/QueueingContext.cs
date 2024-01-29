c596bf967f3728a1504345e36f01438f8ed1e9d7
Vadzim Kurdzesau <vkurdzesau@gmail.com>
2024-01-30 00:03:23 +0100

    feat: add dbcontext

---
+++ src/Enqueuer.Queueing.Infrastructure/Persistence/QueueingContext.cs
@@ -0,0 +1,17 @@
+﻿using Enqueuer.Queueing.Domain.Models;
+using Enqueuer.Queueing.Infrastructure.Persistence.EntityConfigurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Enqueuer.Queueing.Infrastructure.Persistence;
+
+public class QueueingContext : DbContext
+{
+    public DbSet<Queue> Queues { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .ApplyConfiguration(new QueueEntityConfiguration())
+            .ApplyConfiguration(new ParticipantEntityConfiguration());
+    }
+}
